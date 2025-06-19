5e40473b6f31a5a8d056be94f5664d978371663a
Zachary Rener <me@zacharyrener.com>
2025-06-18 21:12:59 -0500

    Add selectable feature settings

--- assets/js/post-types/index.js
+++ assets/js/post-types/index.js
@@ -52,14 +52,69 @@ const App = () => {
 		return () => form.removeEventListener('submit', submit);
 	}, [currentPostType.settingsSchema, values]);
 
+	/**
+	 * Determines whether a control should be rendered based on its requirements.
+	 *
+	 * @param {object} requires_fields An object representing the required field values for rendering.
+	 * Can contain 'conditions' object with field requirements and 'relationship' key ('AND' or 'OR').
+	 * @returns {boolean} Returns `true` if the control should be rendered, otherwise `false`.
+	 */
+	const shouldRenderControl = (requires_fields) => {
+		if (!requires_fields || Object.keys(requires_fields).length === 0) {
+			return true;
+		}
+
+		// Get field requirements from 'conditions' key
+		let fieldRequirements;
+
+		if (requires_fields.conditions) {
+			fieldRequirements = Object.entries(requires_fields.conditions);
+		}
+
+		// If no actual field requirements, return true
+		if (fieldRequirements.length === 0) {
+			return true;
+		}
+
+		// Define the condition check function
+		const checkCondition = ([fieldKey, requiredValue]) => {
+			const actualValue = values[fieldKey];
+			// const defaultValue = defaultSettings[fieldKey] ?? false;
+			return actualValue === requiredValue;
+		};
+
+		// Extract relationship type, default to 'AND'
+		const relationship = (requires_fields.relationship || 'AND').toUpperCase();
+
+		// Apply the appropriate logic based on relationship type
+		switch (relationship) {
+			case 'OR':
+				return fieldRequirements.some(checkCondition);
+			case 'AND':
+			default:
+				// Default to AND for any unexpected values
+				return fieldRequirements.every(checkCondition);
+		}
+	};
+
 	return (
 		<div>
 			{currentPostType.settingsSchema.map((schema) => {
+				/**
+				 * Skip rendering if the control should not be rendered based on requires_fields.
+				 */
+				if (!shouldRenderControl(schema.requires_fields)) {
+					return null;
+				}
+
+				const value =
+					typeof values[schema.key] !== 'undefined' ? values[schema.key] : schema.default;
+
 				return (
 					<Control
 						type={schema.type}
 						settings={schema}
-						value={values[schema.key] || ''}
+						value={value}
 						onChange={(val) => setValues((prev) => ({ ...prev, [schema.key]: val }))}
 					/>
 				);
--- includes/classes/PostType/Bot.php
+++ includes/classes/PostType/Bot.php
@@ -11,6 +11,8 @@ namespace ElasticPressLabs\PostType;
 use ElasticPressLabs\PostTypes;
 use ElasticPressLabs\PostType;
 
+use ElasticPress\Features;
+
 /**
  * Comments feature class
  */
@@ -56,152 +58,55 @@ class Bot extends PostType {
 	 * @since 5.3.0
 	 */
 	protected function set_settings_schema() {
-		$this->settings_schema = [
+		$store    = Features::factory();
+		$features = $store->registered_features;
+
+		$selectable_features = [ 'search', 'instant-results', 'autosuggest', 'did-you-mean', 'facets' ];
+
+		$options = [
 			[
-				'key'     => 'search_term_embed_method',
-				'label'   => __( 'Search Term Embedding Method', 'elasticpress-labs' ),
-				'help'    => __( 'The method to use to vectorize the search term. The model used here should match the one used to vectorize your content.', 'elasticpress-labs' ),
-				'options' => [
-					[
-						'label' => __( 'Client side', 'elasticpress-labs' ),
-						'value' => 'client-side',
-					],
-					[
-						'label' => __( 'Server side', 'elasticpress-labs' ),
-						'value' => 'server-side',
-					],
-				],
-				'type'    => 'radio',
-			],
-			[
-				'key'     => 'api_key',
-				'label'   => __( 'OpenAI API Key', 'elasticpress-labs' ),
-				'help'    => sprintf(
-					wp_kses(
-						/* translators: %1$s: OpenAI sign up URL */
-						__( 'Don\'t have an OpenAI account yet? <a title="Sign up for an OpenAI account" href="%1$s">Sign up for one</a> in order to get your API key.', 'elasticpress-labs' ),
-						[
-							'a' => [
-								'href'  => [],
-								'title' => [],
-							],
-						]
-					),
-					esc_url( 'https://platform.openai.com/signup' )
-				),
-				'type'    => 'text',
-				// 'default' => $this->default_settings['api_key'],
-			],
-			[
-				'key'     => 'api_url',
-				'help'    => __( 'OpenAI Chat Completion API Url', 'elasticpress-labs' ),
-				'label'   => __( 'OpenAI Chat Completion API Url', 'elasticpress-labs' ),
-				'type'    => 'text',
-				// 'default' => $this->default_settings['api_url'],
-			],
-			[
-				'key'     => 'chat_model',
-				'help'    => __( 'OpenAI Chat model', 'elasticpress-labs' ),
-				'label'   => __( 'The name of the chat model to use', 'elasticpress-labs' ),
-				'type'    => 'text',
-				// 'default' => $this->default_settings['chat_model'],
-			],
-			[
-				'key'     => 'number_of_posts',
-				'label'   => __( 'Number of posts', 'elasticpress-labs' ),
-				'help'    => __( 'Number of posts to be used in the context building', 'elasticpress-labs' ),
-				'type'    => 'number',
-				// 'default' => $this->default_settings['number_of_posts'],
-			],
-			[
-				'key'     => 'prompt',
-				'label'   => __( 'AI Prompt', 'elasticpress-labs' ),
-				'help'    => __( 'The <code>{posts}</code> string will be replaced.', 'elasticpress-labs' ),
-				'type'    => 'textarea',
-				// 'default' => $this->default_settings['prompt'],
-			],
-			[
-				'default' => '0',
-				'key'     => 'trigger_ga_event',
-				'help'    => __( 'Enable to fire a gtag tracking event when an autosuggest result is clicked.', 'elasticpress' ),
-				'label'   => __( 'Trigger Google Analytics events', 'elasticpress' ),
-				'type'    => 'checkbox',
-			],
-			[
-				'key'   => 'epio',
-				'label' => sprintf(
-					/* translators: 1: <a> tag (ElasticPress.io); 2. </a>; 3: <a> tag (KB article); 4. </a>; 5: <a> tag (Site Health Debug Section); 6. </a>; */
-					__( 'You are directly connected to %1$sElasticPress.io%2$s, ensuring the most performant Autosuggest experience. %3$sLearn more about what this means%4$s or %5$sclick here for debug information%6$s.', 'elasticpress' ),
-					'<a href="123">',
-					'</a>',
-					'<a href="123">',
-					'</a>',
-					'<a href="123">',
-					'</a>'
-				),
-				'type'  => 'markup',
-			],
-			[
-				'default' => 'post_type,tax-category,tax-post_tag',
-				'key'     => 'facets',
-				'label'   => __( 'Filters', 'elasticpress' ),
-				'options' => [
-					[
-						'label' => 'Post type',
-						'value' => 'post_type',
-					],
-					[
-						'label' => 'Category (category)',
-						'value' => 'tax-category',
-					],
-					[
-						'label' => 'Tag (post_tag)',
-						'value' => 'tax-post_tag',
-					],
-				],
-				'type'    => 'multiple',
-			],
-			[
-				'default' => 'mark',
-				'help'    => __( 'Select the HTML tag used to highlight search terms.', 'elasticpress' ),
-				'key'     => 'highlight_tag',
-				'label'   => __( 'Highlight tag', 'elasticpress' ),
-				'options' => [
-					[
-						'label' => __( 'None', 'elasticpress' ),
-						'value' => '',
-					],
-					[
-						'label' => 'mark',
-						'value' => 'mark',
-					],
-					[
-						'label' => 'span',
-						'value' => 'span',
-					],
-					[
-						'label' => 'strong',
-						'value' => 'strong',
-					],
-					[
-						'label' => 'em',
-						'value' => 'em',
-					],
-					[
-						'label' => 'i',
-						'value' => 'i',
-					],
-				],
-				'type'    => 'select',
-			],
-			[
-				'default'          => false,
-				'key'              => 'active',
-				'label'            => __( 'Enable', 'elasticpress' ),
-				// 'requires_feature' => $this->requires_feature,
-				// 'requires_sync'    => $this->requires_install_reindex,
-				'type'             => 'toggle',
+				'label' => __( 'None', 'elasticpress' ),
+				'value' => '',
 			],
 		];
+
+		foreach ( $selectable_features as $slug ) {
+			$feature          = $features[ $slug ];
+			$feature_settings = $feature->get_settings_schema();
+
+			$options[] = [
+				'label' => $feature->title,
+				'value' => $feature->slug,
+			];
+		}
+
+		$feature_settings_all = [];
+		foreach ( $selectable_features as $slug ) {
+			$feature          = $features[ $slug ];
+			$feature_settings = array_slice( $feature->get_settings_schema(), 1 );
+
+			foreach ( $feature_settings as &$setting ) {
+				$setting['requires_fields'] = [
+					'conditions' => [
+						'feature_selection' => $slug,
+					],
+				];
+			}
+			unset( $setting );
+
+			$feature_settings_all = array_merge( $feature_settings_all, $feature_settings );
+		}
+
+		$this->settings_schema = [
+			[
+				'help'    => __( 'Select an AI-enabled ElasticPress Feature to override its configuration.', 'elasticpress' ),
+				'key'     => 'feature_selection',
+				'label'   => __( 'Feature Selection', 'elasticpress' ),
+				'options' => $options,
+				'type'    => 'select',
+			],
+		];
+
+		$this->settings_schema = array_merge( $this->settings_schema, $feature_settings_all );
 	}
 }
