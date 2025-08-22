10245778146445bb65a3e6b87a9a7a200467ffe5
Felipe Elia <felipe.elia@get10up.com>
2025-08-22 11:41:12 -0300

    Initial version of an e2e test for Vector Embeddings

---
+++ tests/e2e/specs/vector-embeddings.spec.ts
@@ -0,0 +1,36 @@
+import {
+	goToAdminPage,
+	wpCli,
+	maybeEnableFeature,
+	test,
+	expect,
+	maybeDisableFeature,
+} from 'elasticpress-playwright-utils';
+
+test.describe('Vector Embeddings Feature', () => {
+	test('Can turn the feature on', async ({ loggedInPage }) => {
+		await maybeDisableFeature('vector_embeddings');
+		await goToAdminPage(loggedInPage, 'admin.php?page=elasticpress');
+
+		await loggedInPage.getByRole('button', { name: 'AI', exact: true }).click();
+		await loggedInPage.getByRole('button', { name: 'Vector Embeddings' }).click();
+		await loggedInPage.getByRole('checkbox', { name: 'Enable' }).click();
+		loggedInPage.on('dialog', (dialog) => dialog.accept());
+		await loggedInPage.getByRole('button', { name: 'Save and sync now' }).click();
+
+		await loggedInPage.getByRole('button', { name: 'Log' }).click();
+		const syncMessages = loggedInPage.locator('.ep-sync-messages');
+		await expect(syncMessages).toContainText('Mapping sent');
+		await expect(syncMessages).toContainText('Sync complete');
+
+		const result = await wpCli('elasticpress list-features');
+		expect(result.toString()).toContain('vector_embeddings');
+
+		await goToAdminPage(loggedInPage, 'admin.php?page=elasticpress-status-report');
+		const vectorEmbeddingsButton = loggedInPage.getByRole('button', { name: 'Vector Embeddings', exact: true }).first();
+		const vectorEmbeddingsGroup = loggedInPage.locator('.components-panel__body', { has: vectorEmbeddingsButton }).first();
+		await vectorEmbeddingsButton.click();
+		await expect(vectorEmbeddingsGroup).toContainText('Content in the queue');
+		await expect(vectorEmbeddingsGroup.locator('td').nth(1)).not.toContainText('0');
+	});
+});
