3c22ce77a6335510fa0f3cd3b9867b611bf31e52
Felipe Elia <felipe.elia@get10up.com>
2025-10-16 12:05:32 -0300

    Fix the other test of search templates...

--- tests/e2e/specs/search-templates.spec.ts
+++ tests/e2e/specs/search-templates.spec.ts
@@ -17,7 +17,8 @@ test.describe('Search Templates Feature', { tag: '@search-templates' }, () => {
 
 		await goToAdminPage(loggedInPage, 'admin.php?page=elasticpress');
 
-		await loggedInPage.locator('button', { hasText: 'Search Templates' }).click();
+		await loggedInPage.getByRole('button', { name: 'Other', exact: true }).click();
+		await loggedInPage.getByRole('button', { name: 'Search Templates' }).click();
 		await expect(
 			loggedInPage.locator(
 				'.components-notice:has-text("You need an ElasticPress.io account")',
